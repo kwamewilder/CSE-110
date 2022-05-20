6085e902a959dd624119615eec8791512ed1a5ce
Emmanuel O. Gyasi-Ampomah <oheneba.ega@Outlook.com>
2022-05-20 22:23:50 +0000

    Weeks Added

---
+++ WEEK 1/favouriteColor.py
@@ -0,0 +1,14 @@
+"""
+File: favouriteColor.py
+Author: Emmanuel Oheneba Gyasi-Ampomah
+
+Purpose: Receive color from user and display it.
+"""
+
+# Take color input from user and store in color
+color = input('Please type your favorite color:')
+
+message = 'Great Choice!'
+
+# Display color with string
+print(message + ' Your favorite color is: ' + color.capitalize() + '.')
